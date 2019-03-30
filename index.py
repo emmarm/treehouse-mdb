1a718124e1dea3f025c40590876dab6e51107e91
mawarijo <mdopure@gmail.com>
2019-03-29 22:02:25 -0700

    add api key to config file, request popular movies

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
+import requests
+
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 # most basic example
@@ -12,3 +15,13 @@ def hello():
 @app.route('/hello/<string:name>', methods=['GET'])
 def say_hello(name):
     return 'Hello, {}'.format(name.title())
+
+
+@app.route('/movies/', methods=['GET'])
+def get_movies():
+    api_key = app.config['TMDB_API_KEY']
+    res = requests.get(
+        f'https://api.themoviedb.org/3/movie/popular?api_key={api_key}&language=en-US&page=1')
+    movie_list = res.json()['results']
+    print(movie_list)
+    return movie_list[0]['title']
